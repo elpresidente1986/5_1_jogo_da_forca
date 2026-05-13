fa975dd3dac4aff72238a5b786ccd4c8a15c56cc
Elpresidente <bfmaguiar@gmail.com>
2026-05-14 00:18:41 +0100

    Define estrutura inicial do projeto

---
+++ forca/jogo.py
@@ -0,0 +1,5 @@
+# Ficheiro com a lógica principal do jogo da forca
+
+def iniciar_jogo():
+    print("Jogo da Forca")
+    print("Estrutura inicial do projeto criada.")
---
+++ forca/main.py
@@ -0,0 +1,7 @@
+# Ficheiro principal do jogo da forca
+
+from jogo import iniciar_jogo
+
+
+if __name__ == "__main__":
+    iniciar_jogo()
